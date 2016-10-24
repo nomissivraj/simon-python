ac502c853ce263ef83278719f06105b3f4770ea7
(s) Simon-Peter Jarvis <sjarvis5@pm7-0272.uopnet.plymouth.ac.uk>
2016-10-24 17:44:22 +0100

    started visualisation

---
+++ visualisation/animation_example.py
@@ -0,0 +1,15 @@
+
+window = GraphWin("Visualisation", 300, 300)
+ball = Circle(Point(100,300), 20)
+ball.setFill(color_rgb(255,255,0))
+xspeed = 1.5
+yspeed = 1.5
+ball.draw(window)
+
+while True:
+    currentPosition = ball.getCenter()
+    if(currentPosition.getX() <= 0 ): xspeed = -xspeed
+    if(currentPosition.getX() >= 300 ): xspeed = -xspeed
+    if(currentPosition.getY() <= 0 ): yspeed = -yspeed
+    if(currentPosition.getY() >= 300 ): yspeed = -yspeed
+    ball.move(xspeed,yspeed)
