8bebaa38024dc75445f967eb46e915e4c7ca9c88
nomissivraj <simonpeterjarvis@gmail.com>
2016-12-08 00:57:05 +0000

    removed unnecessary comments

--- twitterpost/twitterpost.py
+++ twitterpost/twitterpost.py
@@ -48,22 +48,3 @@ while True:
 
     #set program to sleep for 1 hour in seconds - interval to repeat at
     time.sleep(3600)
-
-
-''' Example to Get stuff from twitter
-#Hardcode a user ID into a variable (Stephen Fry)
-user = 15439395
-
-#Load in my keys and secrets from the credentials file into a list (array)
-file = open("twitterCredentials.txt")
-creds = file.read().split('\n')
-
-#Create a new API wrapper, passing in my credentials one at a time
-api = twitter.Api(creds[0],creds[1],creds[2],creds[3])
-
-#Get the most recent batch of status updates from the user
-statuses = api.GetUserTimeline(user)
-
-#Just print out the most recent one
-print (statuses[0].text)
-'''
